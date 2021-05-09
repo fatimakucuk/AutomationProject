8b562e4d7044f7ea740ead1ae7df49d7b66b57f4
fatimakucuk <72092041+fatimakucuk@users.noreply.github.com>
2021-05-09 19:44:29 +0200

    I have updated hello.java file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        System.out.println("hello clASS eu3");
+        //more changes
+        //i am ready to commit this
     }
 }
